b7c041b5f041169d1756048c1b720d3326e6b808
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-05-06 23:38:39 +0530

    Update ypayment.jsx

--- src/components/ypayment.jsx
+++ src/components/ypayment.jsx
@@ -1,6 +1,5 @@
 import "./ypayment.css";
 
-// export default function YpaymentDetails() {
 export const YpaymentDetails = (ypayment) => {
   return (
     <>
@@ -16,34 +15,38 @@ export const YpaymentDetails = (ypayment) => {
             <p class="subscriptionModel"> Subscribe to Premium - v1.5 (yearly)</p>
             <div class="amount">
               $ <span> </span>USD
-              {/* {ypayment.price} */}
+              {ypayment.price}
             </div>
           </div>
+
           <div class="premiumPayPage">
             <h3>Premium - v1.5(yearly) </h3>
             <h3 id="price1">
               $ <span> </span>
-              {/* {ypayment.price} */}
+              {ypayment.price}
             </h3>
           </div>
+
           <div id="toatlAmt">
             <h3>Subtotal</h3>
             <h3 id="price2">
               $ <span> </span>
-              {/* {ypayment.price} */}
+              {ypayment.price}
             </h3>
           </div>
+
           <hr />
           <div id="tax">
             <p>Tax</p>
             <p>$0.00</p>
           </div>
           <hr />
+
           <div id="dueAmt">
             <h3>Total due today </h3>
             <h3 id="price3">
               $ <span> </span>
-              {/* {ypayment.price} */}
+              {ypayment.price}
             </h3>
           </div>
         </div>
