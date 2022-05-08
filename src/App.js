5efd72ec49b7b44b3de5388053861ce8f2aacd8c
Manjari <raikwar.manjari@gmail.com>
2022-05-08 22:03:04 +0530

    Create page commited

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Navbar from './components/Navbar';
-import SignUp from './components/Signup';
-import Login from './components/Login';
-import Home from './components/Home';
-import Footer from './components/Footer';
-import Copyright from './components/Copyright';
+import "./components/CreatePage.css";
+import {CreatePage} from './components/CreatePage'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     <Router>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" exact element={<Home/>}></Route>
-          <Route path="/login" exact element={<Login/>}></Route>
-          <Route path="/signup" exact element={<SignUp/>}></Route>
-        </Routes>
-        <Footer></Footer>
-        <Copyright></Copyright>
-      </Router>
+      {/* <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2> */}
+      <CreatePage />
     </div>
   );
-}   
-
-export default App;
+}
---
+++ src/components/CreatePage.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { FaImages } from 'react-icons/fa';
+import { FaMusic } from 'react-icons/fa';
+import { FaTags } from 'react-icons/fa';
+import { FaPager} from 'react-icons/fa';
+import { FaLink} from 'react-icons/fa';
+import {FaPlusCircle} from 'react-icons/fa';
+import {Link} from "react-router-dom";
+
+export  const CreatePage = ()=>{
+  return (
+    <>
+    {/* Navbar of create page */}
+    <div className="create-navbar">
+    
+        <div className="nav">
+            <nav className="publish_navbar">
+                <div className="publish-nav-lefthalf">
+                <Link to="/" className="publish-navbar-logo"><img className="publish-lumen-icon" src="https://storage.googleapis.com/lumen5-site-images/L5-logo/L5-logo-header.png" alt="lumen icon"/></Link>
+                </div>
+                <div className="publish-nav-righthalf">
+                    <div className="upgrade">
+                        <button className="upgrade-btn1">PUBLISH</button>
+                    </div>
+                    <div className="notification-user">
+                        <div className="notification"><i className="fa-solid fa-bell n-icon-color"></i></div>
+                        <div className="user">user name</div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+  
+
+    </div> 
+
+    {/* Main div of create page */}
+    <div className="create-maindiv">
+
+      {/* Create page side bar */}
+      <div className="create-sidebar">
+         <div className="create-sidecomp">
+           <FaBars />
+           <br></br>
+           Story</div>
+         <div className="create-sidecomp">
+           <FaImages/>
+           <br></br>
+           Media</div>
+         <div className="create-sidecomp">
+           <FaMusic/>
+           <br></br>
+           Music</div>
+         <div className="create-sidecomp">
+           <FaTags/>
+           <br></br>
+           Style</div>
+         <div className="create-sidecomp">
+           <FaPager/>
+           <br></br>
+           Format</div>
+      </div>
+
+      {/* Create page middle div */}
+      <div className="create-middlediv">
+        <div className="create-middletop">
+          <div className="create-middletopdiv">
+          <FaLink />
+            <input className="create-middleinput" placeholder="Place a URL to your blog/article"></input>
+          </div>
+          <button className="create-middlebutton">Import</button>
+        </div>
+        <div className="create-middlebottom">
+          <p>Welcome to Lumen5!</p>
+          
+          <p>PREPARE YOUR SCRIPT</p>
+          <p>Paste the URL from your article or blog in the field above or copy/paste text directly in this text box.</p>
+          
+          <p>BUILD YOUR VIDEO WITH SCENES</p>
+          <p>Click the green (+)button or drag text and media into the editor to create new scenes.</p>
+          
+          <p>POPULATE YOUR SCENES WITH MEDIA</p>
+          <p>Drag images and videos from the media tab on to your scenes. Upload your own or select from our stock media library</p>
+          
+          <p>Can't find the right image? No worries - we offer many scene designs that that convey your message with words and graphics only.</p>
+        </div>
+        <div>
+          <button>Convert to video</button>
+        </div>
+      </div>
+
+      {/* Create page last div */}
+      <div className="create-lastdiv">
+        <div className="create-lastrecord"></div>
+        <div className="create-lastdivtop">
+
+          <div className="create-lasticon"></div>
+          <div className="create-lastdivimage">
+            <img src="https://storage.googleapis.com/lumen5-prod-lottie/Tracer/Preview/Tracer_Thumbnail_4a.png" alt=""/>
+          </div>
+          <div className="create-lastbutton">
+            <button>Preview</button>
+            <button>+  6s  -</button>
+            <button>Swap design</button>
+            <button>Suffle colors</button>
+          </div>
+
+        </div>
+        <div className="create-lastdivplus">
+          <FaPlusCircle  className="create-circle"/>
+        </div>
+        <div className="create-lastdivouter"></div>
+        <div className="create-lastdivbottom">
+
+        </div>
+      </div>
+
+    </div>
+    </>
+  )
+}
