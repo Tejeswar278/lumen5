c5f0f7b0d55f321716f08547267d1aca699bd740
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-05-09 13:52:05 +0530

    publish css updated

--- src/components/Publish.js
+++ src/components/Publish.js
@@ -3,7 +3,6 @@ import "./Publish.css"
 import {FaCloudUploadAlt} from "react-icons/fa"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import NavbarLandingpage from "./NavbarLandingpage"
 
 const Publish_Navbar = () => {
     const navigate = useNavigate();
@@ -28,6 +27,7 @@ const Publish_Navbar = () => {
 }
 
 export const Publish = ()=> {
+    const navigate = useNavigate();
 
     const [comment, setCommet] = useState("")
     const [comments, setComments] = useState([])
@@ -48,9 +48,9 @@ export const Publish = ()=> {
                     <div className="block">
                         <div className="div1">
                             <div>Upgrade to remove Lumen5 credit scene <i className="fa-solid fa-circle-question circle-color"></i></div>
-                            <div><button className="upgrade-btn2">Upgrade</button></div>
+                            <div><button className="upgrade-btn2" onClick={()=> {navigate("/pricing")}}>Upgrade</button></div>
                         </div>
-                        <div className="div2"></div>
+                        <div className="div2"><img src="https://storage.googleapis.com/lumen5-prod-lottie/Tracer/Preview/Tracer_Thumbnail_4a.png" alt="imageicon"/></div>
                         <div className="div3">00:00</div>
                         <div className="div4">
                             <div className="div4-1">Copy of Untitled video</div>
@@ -58,13 +58,15 @@ export const Publish = ()=> {
                                 <div className="div4-2-1">
                                     <div className="div4-2-1-1">M</div>
                                     <div>
-                                        <div className="div4-2-1-2">Muddada name</div>
+                                        <div className="div4-2-1-2">
+                                            Ajinkya
+                                        </div>
                                         <div>Published on date</div>
                                     </div>
                                 </div>
                                 <div className="div4-2-2">
                                     <div><button className="upgrade-btn1">Edit video</button></div>
-                                    <div className="div4-2-2-2"><button className="upgrade-btn2">Download</button></div>
+                                    <div className="div4-2-2-2"><button className="upgrade-btn2 ugbtn2">Download</button></div>
                                 </div>
                             </div>
                         </div>
@@ -83,14 +85,14 @@ export const Publish = ()=> {
                     </div>
                 </div>
                 <div className="right-body">
-                    <div>Comment Scene</div>
+                    <div><h2>Comment Scene</h2></div>
                     <div>
                         {comments.map((e,i) => (
-                            <p key={i}>{e}</p>
+                            <p key={i}>{i+1}. {e} </p>
                         ))}
                     </div>
-                    <input value={comment || ""} type="text" onChange={(e) => setCommet(e.target.value)} placeholder="Enter comment"/>
-                    <button onClick={handleSubmit} >Add comment</button>
+                    <input className="inputtag" value={comment || ""} type="text" onChange={(e) => setCommet(e.target.value)} placeholder="Enter comment"/>
+                    <button onClick={handleSubmit} className="upgrade-btn2 p-btn">Add comment</button>
                 </div>
             </div>
         </>
