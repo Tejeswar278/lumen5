df27fb96fbae8101bf86c65dfb2fe22c23a38447
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-05-09 15:56:46 +0530

    Final

--- src/components/Thankyou.jsx
+++ src/components/Thankyou.jsx
@@ -1,21 +1,24 @@
 import "./thankyou.css";
+import { useNavigate } from "react-router-dom";
+import { Button } from "./Button";
 
 export default function Thankyou() {
+  const navigate = useNavigate();
   return (
-    // <>
-    <div id="container">
+    <div className="container">
       <center>
-        <img
-          src="https://w7.pngwing.com/pngs/264/251/png-transparent-check-mark-checkbox-computer-icons-resort-green-tick-icon-check-illustration-miscellaneous-angle-leaf.png"
-          alt=""
-        />
-        <h1 style="color: #82C815;">Thank You For subscription!</h1>
-        <h3>Your subscription has been done</h3>
-        <h3>
-          <a onclick="window.location.href='Thankyou.jsx'">Go Back</a>
-        </h3>
+        <img className="tyimg" src="https://yachtpon.com/wp-content/uploads/2019/03/5a142d709c.png" alt="" />
+        <h1 className="tycl">Thank You For subscription!</h1>
+        <h3 className="h1siva">Your Transaction has been done!</h3>
+        <button
+          className="tybtn"
+          onClick={() => {
+            navigate("/template");
+          }}
+        >
+          Go to template page
+        </button>
       </center>
     </div>
-    // </>
   );
 }
