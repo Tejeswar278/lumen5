5cc387a41f66d595d11c2bfb232625e81149542e
Vivek-Badole <97503546+Vivek-Badole@users.noreply.github.com>
2022-05-09 11:13:01 +0530

    Vivek

---
+++ src/components/button2.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Button2 = styled.button`
+  border: 1px solid #143f6b;
+  border-radius: 30px;
+  font-size: 16px;
+  padding: 0px 20px 0px 20px;
+  /* padding-top: px; */
+  text-align: center;
+  background: white;
+  color: #5846f6;
+  cursor: pointer;
+  margin: 5px;
+
+  &:hover {
+    background-color: #5846f6;
+    color: white;
+  }
+`;
+
+export { Button2 };
